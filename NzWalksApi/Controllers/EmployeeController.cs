36cb22e505f80d90624da17937657c6905778526
udaymathur14 <udaymathur14@gmail.com>
2023-12-07 13:20:51 +0530

    Finish EmployeeDetails Controller BAsics

--- NzWalksApi/Controllers/EmployeeController.cs
+++ NzWalksApi/Controllers/EmployeeController.cs
@@ -40,6 +40,67 @@ public class EmployeeController : ControllerBase
         var employees = employeeDbContext.Employee.ToList();
         return Ok(employees);
     }
+    [HttpGet]
+    [Route("{id:int}")]
+    public IActionResult GetEmployeeById([FromRoute] int id)
+    {
+        //Normal method 
+        //Find method will only take the primary key not other key 
+        var employee = employeeDbContext.Employee.Find(id);
+
+        //linq method 
+       // var employee = employeeDbContext.Employee.FirstOrDefault(x=>x.EmployeeID == id );
+        
+        if(employee == null)
+        {
+            return NotFound();
+        }
+        return Ok(employee);
+    }
+
+    [HttpPost]
+
+    public IActionResult Create([FromBody] EmployeeDetails employeeDetails)
+    {
+        employeeDbContext.Employee.Add(employeeDetails);
+        employeeDbContext.SaveChanges();
+        return CreatedAtAction(nameof(GetEmployeeById), new { id = employeeDetails.EmployeeID }, employeeDetails);
+        //return Ok();
+
+    }
+
+    [HttpPut]
+    [Route("{id:int}")]
+    public async Task<IActionResult> UpdateEmployeeDetails([FromRoute] int id, [FromBody] EmployeeDetails updateEmployeeDetails)
+    {
+
+        // Map DTO to Domain Model
+        var employeExists = employeeDbContext.Employee.Find(id);
+        if (employeExists == null)
+        {
+            return NotFound();
+        }
+        employeExists.FirstName = updateEmployeeDetails.FirstName;
+        // Check if region exists
+        //updateEmployeeDetails = await employeeDbContext.UpdateAsync(id, employeExists);
+        employeeDbContext.SaveChanges();return Ok();
+
+
+    }
+
+    [HttpDelete]
+    [Route("{id:int}")]
+    public IActionResult DeleteEmployee([FromRoute]int id) { 
+
+        var employeeexist = employeeDbContext.Employee.Find(id);
+        if(employeeexist == null)
+        {
+            return NotFound();
+        }
+        employeeDbContext.Employee.Remove(employeeexist);
+        employeeDbContext.SaveChanges();    
+        return Ok();
+    }
 };
 
 
