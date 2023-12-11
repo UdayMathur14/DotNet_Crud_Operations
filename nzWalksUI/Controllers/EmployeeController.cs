b0eb572a84fec4016f01f8e14215e0a4262a2288
udaymathur14 <udaymathur14@gmail.com>
2023-12-11 17:02:59 +0530

    add dot net we app mvc

---
+++ nzWalksUI/Controllers/EmployeeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace nzWalksUI.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //To consume this web api , get the information nd then pss the information view 
+            //in asp.ne core there is  framework known s http client which comes from system dot net http 
+            //namespace
+
+            //It provides us a way to consume the web api or different types of api , 
+            return View();
+        }
+    }
+}
